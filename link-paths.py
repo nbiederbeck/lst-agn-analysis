688f75ebb3745de94dccf85d49579fbbb7adfef0
Noah Biederbeck <noah.biederbeck@tu-dortmund.de>
2022-10-24 13:34:51 +0000

    Loop over sources, sort into sources

--- link-paths.py
+++ link-paths.py
@@ -2,34 +2,51 @@ from pathlib import Path
 
 import numpy as np
 import tables
+from tqdm import tqdm
 
-from run_ids import mrk421
+from run_ids import mrk421, one_es1959
 
-outdir = "build/dl1/{zd}/"
+sources = {
+    "mrk421": mrk421,
+    "one_es1959": one_es1959,
+}
+
+outdir = "build/dl1/{source}/{zd}/"
 filename = "dl1_LST-1.Run{run_id:05d}.h5"
 template_target = "/fefs/aswg/data/real/DL1/{night}/v0.9/tailcut84/" + filename
 template_linkname = outdir + filename
 
-Path(outdir).mkdir(parents=True, exist_ok=True)
 
 if __name__ == "__main__":
-    for night, run_ids in mrk421.items():
-        for run_id in run_ids:
-            target = template_target.format(night=night, run_id=run_id)
+    for name, source in tqdm(sources.items()):
+        for night, run_ids in tqdm(source.items(), position=1):
+            for run_id in tqdm(run_ids, position=2):
+                target = template_target.format(night=night, run_id=run_id)
 
-            # check mean zenith distance
-            with tables.open_file(
-                target, root_uep="/dl1/event/telescope/parameters"
-            ) as f:
-                zd_mean = 90 - np.rad2deg(np.mean(f.root.LST_LSTCam.col("alt_tel")))
+                # check mean zenith distance
+                with tables.open_file(
+                    target,
+                    root_uep="/dl1/event/telescope/parameters",
+                    mode="r",
+                ) as f:
+                    zd_mean = 90 - np.rad2deg(
+                        f.get_node("/LST_LSTCam").col("alt_tel").mean()
+                    )
 
-            print(zd_mean)
+                if zd_mean < 30:
+                    zd = "zenith_20deg"
+                else:
+                    zd = "zenith_40deg"
 
-            if zd_mean < 30:
-                zd = "low"
-            else:
-                zd = "high"
+                linkname = Path(
+                    template_linkname.format(
+                        night=night,
+                        run_id=run_id,
+                        zd=zd,
+                        source=name,
+                    )
+                )
 
-            linkname = template_linkname.format(night=night, run_id=run_id, zd=zd)
-
-            Path(linkname).symlink_to(target)
+                if not linkname.is_file():
+                    linkname.parent.mkdir(exist_ok=True, parents=True)
+                    linkname.symlink_to(target)
