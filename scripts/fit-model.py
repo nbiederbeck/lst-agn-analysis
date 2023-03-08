9f999cbcc596f44080fc6d669641ce12b56380f0
Noah Biederbeck <noah.biederbeck@tu-dortmund.de>
2023-03-08 09:24:25 +0100

    fix imports

--- scripts/fit-model.py
+++ scripts/fit-model.py
@@ -1,15 +1,14 @@
 from argparse import ArgumentParser
 
+from gammapy.analysis import Analysis, AnalysisConfig
+from gammapy.datasets import Datasets
+
 parser = ArgumentParser()
 parser.add_argument("-c", "--config", required=True)
 parser.add_argument("--dataset-path", required=True)
 parser.add_argument("--model-config", required=True)
 parser.add_argument("-o", "--output", required=True)
 args = parser.parse_args()
-
-
-from gammapy.analysis import Analysis, AnalysisConfig
-from gammapy.datasets import Datasets
 
 
 def main(config, dataset_path, model_config, output):
