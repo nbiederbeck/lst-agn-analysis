7c1bf8e459f27663ed246e9013603fa0e6485189
Noah Biederbeck <noah.biederbeck@tu-dortmund.de>
2023-04-04 11:42:43 +0200

    Cleaner ticks and labels

--- scripts/plot_light_curve.py
+++ scripts/plot_light_curve.py
@@ -1,13 +1,13 @@
 from argparse import ArgumentParser
 
+from gammapy.estimators import FluxPoints
+from matplotlib import pyplot as plt
+from matplotlib.dates import ConciseDateFormatter
+
 parser = ArgumentParser()
 parser.add_argument("-i", "--input-path", required=True)
 parser.add_argument("-o", "--output", required=True)
 args = parser.parse_args()
-
-
-from gammapy.estimators import FluxPoints
-from matplotlib import pyplot as plt
 
 
 def main(input_path, output):
@@ -15,6 +15,12 @@ def main(input_path, output):
 
     fig, ax = plt.subplots()
     lc.plot(ax=ax)
+    ax.set_ylabel(
+        r"$\Phi \:\:/\:\: "
+        + r"\si{\per\centi\meter\squared\per\second\per\tera\electronvolt}$",
+    )
+    ax.set_xlabel("Date")
+    ax.xaxis.set_major_formatter(ConciseDateFormatter(ax.xaxis.get_major_locator()))
     fig.savefig(output)
 
 
