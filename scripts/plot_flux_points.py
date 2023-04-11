089b7fb7650545bf2dc585100911985104d8ad31
Lukas Nickel <lukas.nickel@tu-dortmund.de>
2023-04-11 15:47:02 +0000

    Make plot scripts more robust

--- scripts/plot_flux_points.py
+++ scripts/plot_flux_points.py
@@ -1,5 +1,7 @@
 from argparse import ArgumentParser
 
+import numpy as np
+
 parser = ArgumentParser()
 parser.add_argument("-i", "--input-path", required=True)
 parser.add_argument("--best-model-path", required=True)
@@ -16,14 +18,34 @@ def main(input_path, best_model_path, output):
     flux_points = FluxPointsDataset.read(input_path)
     flux_points.models = Models.read(best_model_path)
 
-    fig, axes = plt.subplots(
-        nrows=2,
-        sharex=True,
-        gridspec_kw=dict(height_ratios=[3, 1], hspace=0),
-    )
-    flux_points.plot_fit(*axes)
+    # See plot_light_curve.py for why this is necessary
+    sed_type = flux_points.data.sed_type_plot_default
+    y = getattr(flux_points.data, sed_type)
+    y_errn, y_errp = flux_points.data._plot_get_flux_err(sed_type=sed_type)
+    if (
+        np.count_nonzero(~np.isnan(y) > 0)
+        or np.count_nonzero(~np.isnan(y_errn) > 0)
+        or np.count_nonzero(~np.isnan(y_errp) > 0)
+    ):
+        # If everything is an upper limit, residuals will be all nan
+        # This will fail because gammapy sets the yaxis limit in the
+        # residual plot based on the max residual, so we need at least one
+        # We can still plot the spectrum and flux points otherwise
+        if np.count_nonzero(flux_points.data.is_ul) > 0:
+            fig, axes = plt.subplots(
+                nrows=2,
+                sharex=True,
+                gridspec_kw=dict(height_ratios=[3, 1], hspace=0),
+            )
+            flux_points.plot_fit(*axes)
+            axes[0].set_xlabel("")
+        else:
+            fig, ax = plt.subplots()
+            flux_points.plot_spectrum(ax=ax)
+    else:
+        # Need an empty plot to satisfy the rule
+        fig, ax = plt.subplots()
 
-    axes[0].set_xlabel("")
     fig.savefig(output)
 
 
