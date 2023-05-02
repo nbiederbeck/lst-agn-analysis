b63ff92a1e23f5cddadf461ed60bda2540ac1ed3
Noah Biederbeck <noah.biederbeck@tu-dortmund.de>
2023-05-02 15:32:34 +0200

    Add labels and coordinates

--- scripts/plot_skymap.py
+++ scripts/plot_skymap.py
@@ -4,6 +4,7 @@
 from argparse import ArgumentParser
 
 import numpy as np
+from astropy import units as u
 from gammapy.maps import WcsMap
 from matplotlib import pyplot as plt
 
@@ -12,6 +13,8 @@ parser.add_argument("-i", "--input-paths", required=True, nargs="+")
 parser.add_argument("-o", "--output-path", required=True)
 args = parser.parse_args()
 
+angle = u.deg
+
 
 def main(input_paths, output_path):
     data = []
@@ -19,14 +22,30 @@ def main(input_paths, output_path):
         skymap = WcsMap.read(path, map_type="wcs")
         data.append(skymap.data)
 
+    geom = skymap.geom
+    center = geom.center_coord
+    source = geom.center_skydir
+
     fig, ax = plt.subplots()
 
     ax.imshow(np.sum(data, axis=0)[0])
 
     ax.scatter(
-        *skymap.geom.coord_to_pix(skymap.geom.center_coord)[:-1],
+        *geom.coord_to_pix(center)[:-1],
         marker="*",
     )
+
+    ax.set_xticks(
+        geom.coord_to_pix(center)[0],
+        labels=[f"{source.ra.to_value(angle):.2f}"],
+    )
+    ax.set_yticks(
+        geom.coord_to_pix(center)[1],
+        labels=[f"{source.dec.to_value(angle):.2f}"],
+    )
+
+    ax.set_xlabel(f"RA / {angle}")
+    ax.set_ylabel(f"Dec / {angle}")
 
     fig.savefig(output_path)
 
