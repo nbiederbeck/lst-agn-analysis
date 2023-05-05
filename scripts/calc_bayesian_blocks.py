2d1ec335d4e8472909f216a8530b618921a7d46f
Lukas Nickel <lukas.nickel@tu-dortmund.de>
2023-05-05 15:48:59 +0000

    fix argument

--- scripts/calc_bayesian_blocks.py
+++ scripts/calc_bayesian_blocks.py
@@ -7,20 +7,18 @@ from gammapy.estimators import FluxPoints
 parser = ArgumentParser()
 parser.add_argument("-i", "--input-path", required=True)
 parser.add_argument("-o", "--output", required=True)
-parser.add_argument("--threshold", default=0.0027)
+parser.add_argument("--threshold", default=0.0027, type=float)
 args = parser.parse_args()
 
 
 def main(input_path, output, threshold):
     lc = FluxPoints.read(input_path, format="lightcurve")
     table = lc.to_table(format="lightcurve", sed_type="flux")
-    print(table.columns)
     t_min = table["time_min"]
     t_max = table["time_max"]
     flux = table["flux"]
     t = (t_min + t_max) / 2
     x = flux.flatten()  # This is shape (n, 1) for some reason
-    # should be 3 sigma
     blocks = bayesian_blocks(t=t, x=x, p0=threshold, fitness="measures")
     t = Table()
     t["start"] = blocks[:-1]
